02dd2524195ea3d55fed320466349dcb0c87a994
vicoolchik <78905683+vicoolchik@users.noreply.github.com>
2021-02-18 18:09:05 +0200

    Add files via upload

---
+++ classwork/lesson1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	//git remote add origin https://github.com/vicoolchik/-.git
+
+
+	/*int a;
+	cin >> a;
+	if ((a % 2 != 0 && a % 3 != 0)&&(a % 5 != 0 && a % 7 != 0)) {
+		cout << a << "-p";
+	}
+	else {
+		cout << a << "-s";
+	}
+	double a, b, c, p, s;
+	cout << "enter a , b , c";
+	cin >> a >> b >>c;
+	p = (a + b + c) / 2;
+	s = pow((p * (p - a) * (p - b) * (p - c)), 0.5);
+	cout << "walue of s = " << s;
+	return 0;
+	float a, b, c;
+	cin >> a>>b>>c;
+	cout << a<<"x^2+(" << b << "x)+(" << c << ")=0" << endl;
+	float d = b * b - 4 * a * c;
+	if (d > 0) {
+		float x1 = ((-b + sqrt(d) / (2 * a)));
+		float x2 = ((-b - sqrt(d) / (2 * a)));
+		cout << "x1=" << x1 << endl << "x2=" << x2 << endl;
+	}
+	else if (d < 0) {
+		cout << "error";
+	}
+	else if (d == 0) {
+		float x = ((-b ) / (2 * a));
+		cout << "x=" << x << endl;
+	}*/
+
+}
